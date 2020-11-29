9ea047b14174cd51da6d3a48baa9dcb2ea00353f
Mxintro <986695652@qq.com>
2020-11-30 01:39:34 +0800

    detail0.6

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,47 +1,31 @@
 // pages/detail/detail.js
+import { getDetail, getRecommend,Goods, GoodsParam, Shop, Sku } from '../../request/detail'
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    topImages: [],
+    goodInfo: {},
+    shopInfo: {},
+    detailInfo: {},
+    paramInfo: {},
+    commentInfo:{},
+    recommendList: [],
+    navBarIndex: 0,
+    skuInfo: {},
+    skuVisible: false,
+    isBuying: false
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this._getDetailData(options.iid)
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
@@ -57,10 +41,23 @@ Page({
 
   },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  async _getDetailData(iid) {
+    try {
+      const res = await getDetail(iid)
+      const data = res.result
+      
+      this.setData({
+        goodInfo: new Goods(data.itemInfo, data.columns, data.shopInfo.services),
+        shopInfo: new Shop(data.shopInfo),
+        detailInfo: data.detailInfo,
+        topImages: data.itemInfo.topImages,
+        paramInfo: new GoodsParam(data.itemParams.info, data.itemParams.rule),
+        commentInfo: data.rate.list ? data.rate : {},
+        skuInfo: new Sku(data.skuInfo)
+      })
+    } catch (err) {
+      console.log(err)
+    }
   }
+  
 })
---
+++ request/detail.js
@@ -0,0 +1,59 @@
+import request from './request'
+
+export function getDetail(iid) {
+  return request({
+    url: '/detail',
+    data: {
+      iid: iid
+    }
+  })
+}
+
+export function getRecommend() {
+  return request({
+    url: '/recommmed'
+  })
+}
+
+export class Goods { 
+  constructor(itemInfo, columns, services) {
+    this.title = itemInfo.title;
+    this.desc = itemInfo.desc;
+    this.newPrice = itemInfo.price;
+    this.oldPrice = itemInfo.oldPrice;
+    this.iid = itemInfo.iid
+    this.discount = itemInfo.discountDesc;
+    this.columns = columns;
+    this.services = services;
+    this.nowPrice = itemInfo.highNowPrice;
+  }
+}
+
+export class GoodsParam {
+  constructor(info, rule) {
+    // 注: images可能没有值(某些商品有值, 某些没有值)
+    this.image = info.images ? info.images[0] : '';
+    this.infos = info.set;
+    this.sizes = rule.tables;
+  }
+}
+
+export class Shop {
+  constructor(shopInfo) {
+    this.logo = shopInfo.shopLogo;
+    this.name = shopInfo.name;
+    this.fans = shopInfo.cFans;
+    this.sells = shopInfo.cSells;
+    this.score = shopInfo.score;
+    this.goodsCount = shopInfo.cGoods
+  }
+}
+
+export class Sku {
+  constructor(skuInfo) {
+    this.price = skuInfo.defaultPrice
+    this.color = skuInfo.props[0].list
+    this.sizes = skuInfo.props[1].list
+    this.skus = skuInfo.skus
+  }
+}
